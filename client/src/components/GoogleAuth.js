bd27fd188f0c4ac3c282d6447eee9aa50e8470fa
Avinash <avi062@gmail.com>
2020-03-04 17:44:25 +0530

    Method login and logout oauth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -21,19 +21,27 @@ export default class GoogleAuth extends Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     };
 
+    logout = () => {
+        return this.auth.signOut();
+    };
+
+    login = () => {
+        return this.auth.signIn();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
             return null;
         } else if (this.state.isSignedIn) {
             return (
-                <button className="ui red google button">
+                <button onClick={this.logout} className="ui red google button">
                     <i className="google icon" />
                     Sign Out
                 </button>
             );
         } else {
             return (
-                <button className="ui red google button">
+                <button onClick={this.login} className="ui red google button">
                     <i className="google icon" />
                     Sign In with Google
                 </button>
